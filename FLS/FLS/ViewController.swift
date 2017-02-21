1c5a1aedc4ee3a10c2be2629280bef1d7cae9ee8
Kila <277014717@qq.com>
2017-02-21 21:45:18 +0800

    init

---
+++ FLS/FLS/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  FLS
+//
+//  Created by LiJunliang on 2017/2/20.
+//  Copyright © 2017年 Kila. All rights reserved.
+//
+
+import UIKit
+import React
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func click(_ sender: UIButton) {
+        NSLog("Hello")
+        let jsCodeLocation = URL(string: "http://192.168.1.104:8081/index.ios.bundle?platform=ios")
+        let mockData:NSDictionary = ["scores":
+            [
+                ["name":"Alex", "value":"42"],
+                ["name":"Joel", "value":"10"]
+            ]
+        ]
+        
+        let rootView = RCTRootView(
+            bundleURL: jsCodeLocation,
+            moduleName: "RNHighScores",
+            initialProperties: mockData as [NSObject : AnyObject],
+            launchOptions: nil
+        )
+        let vc = UIViewController()
+        vc.view = rootView
+        self.present(vc, animated: true, completion: nil)
+    }
+
+
+}
+
